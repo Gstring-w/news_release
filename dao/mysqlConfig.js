83a0b23e2df27d70d640ab9ae1736bda66a9a78b
管 <37607986+Gstring-w@users.noreply.github.com>
2019-04-18 12:30:31 +0800

    Update mysqlConfig.js

--- dao/mysqlConfig.js
+++ dao/mysqlConfig.js
@@ -2,8 +2,8 @@ const mysqlConfig = {
   host: "localhost",
   port: "3306",
   user: "root",
-  // password: "new password",
-  password: "123456",
+   password: "new password",
+  //password: "123456",
   database: "new_release"
 };
 
